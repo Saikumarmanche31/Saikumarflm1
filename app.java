6d4f1b2e3f86765a92d6ad7ed89a64c89ce02930
Saikumarmanche31 <156987925+Saikumarmanche31@users.noreply.github.com>
2025-08-04 21:44:55 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,11 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+        int num1 = 5; // Declare and initialize integer variable num1
+        int num2 = 10; // Declare and initialize integer variable num2
+        int sum; // Declare integer variable sum
+
+        sum = num1 + num2; // Calculate the sum
+
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum); 
+    }
+}
